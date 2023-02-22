7c2995e7a3735a4ddffe7ec13fba11b510a52e7e
Harminder Singh Nijjar <110620707+harmindersinghnijjar@users.noreply.github.com>
2023-02-22 02:06:07 -0800

    Add files via upload

---
+++ Drop Inventory Items/Dropping all items/src/DropAllItems.java
@@ -0,0 +1,21 @@
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+
+// Script manifest is required to provide details about your script.
+@ScriptManifest(name = "Drop All Items", description = "Drop all coins, drop all items in inventory except axe.", author = "Harminder Singh Nijjar",  version = 0.1,
+        category = Category.MISC, image = "")
+
+public class DropAllItems extends AbstractScript {
+
+    @Override
+    public int onLoop() {
+        Inventory.dropAll("Coins"); // Name
+        Inventory.dropAll(995); // ID
+
+        //Filter that will drop all items that aren't an axe
+        Inventory.dropAll(item -> !item.getName().contains("axe"));
+        return 0;
+    }
+}
