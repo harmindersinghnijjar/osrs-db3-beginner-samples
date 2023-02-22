79b1931ae41286234cf1624e689e0c52bdf6b69e
Harminder Singh Nijjar <110620707+harmindersinghnijjar@users.noreply.github.com>
2023-02-22 15:59:21 -0800

    Add files via upload

---
+++ Pick Up Ground Items/Pick up an item by name/src/PickUpItemsByName.java
@@ -0,0 +1,28 @@
+package src;
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.item.GroundItems;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.items.GroundItem;
+
+// Script manifest is required to provide details about your script.
+@ScriptManifest(name = "Name-Based Item Pickup", description = "Pick up coins by name.", author = "Harminder Singh Nijjar",  version = 0.1,
+        category = Category.MISC, image = "")
+
+public class PickUpItemsByName extends AbstractScript {
+
+    @Override
+    public int onLoop() {
+
+        GroundItem item = GroundItems.closest("Coins");
+
+        if (item != null) {
+            item.interact("Take");
+            Sleep.sleep(2000, 3000);
+        }
+        return 0;
+    }
+}
